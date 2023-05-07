4443514dcf05c9a2978775201a46363c2648c8e4
Einzfeld <126841477+Einzfeld@users.noreply.github.com>
2023-05-07 21:41:20 +0800

    Add files via upload

---
+++ lianxi6.js
@@ -0,0 +1,37 @@
+
+
+function sayHello(){
+  var response=prompt("What is your name?");
+  alert("Hello,"+response+",have a nice day!让我们来猜一猜下图的名称吧！点击正确的选项标题的字会变成相应的颜色，点击后记得重置哦~");
+}
+
+function add(a,b){
+  return a+b;
+}
+console.log(add(20,30));
+console.log(add(200,300));
+
+function showTopic(){
+  var x=document.getElementById('demo');
+  x.style.fontSize="25px";
+  x.style.color="red";
+}
+
+function bianSe(){
+  var x=document.getElementById('1');
+  x.style.fontSize="50px";
+  x.style.color="green";
+}
+function bianSe2(){
+  var x=document.getElementById('2');
+  x.style.fontSize="50px";
+  x.style.color="red";
+}
+function bianSe3(){
+  var x=document.getElementById('2');
+  x.style.fontSize="30px";
+  x.style.color="black";
+    var x=document.getElementById('1');
+    x.style.fontSize="30px";
+    x.style.color="black";
+}
